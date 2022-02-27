bda8795f500d08e65b97ca2c30f5f758db1831d2
Muhammad Aliansyah <aliansyaahian@gmail.com>
2022-02-27 10:25:03 +0700

    Kuis object, array, map

---
+++ strukturData/kuisObject.js
@@ -0,0 +1,48 @@
+/**
+ * TODO
+ * 1. Buatlah variabel dengan nama restaurant yang bertipe object dengan ketentuan berikut:
+ *    - Memiliki properti bernama "name"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti bernama "city"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "favorite drink"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "favorite food"
+ *       - Bertipe data string
+ *       - Bernilai apa pun, asalkan tidak string kosong atau null.
+ *    - Memiliki properti "isVegan"
+ *       - Bertipe data boolean
+ *       - Bernilai boolean apa pun.
+ *
+ * 2. Buatlah variabel bernama name.
+ *    Kemudian isi dengan nilai name dari properti object restaurant
+ * 3. Buatlah variabel bernama favoriteDrink.
+ *    Kemudian isi dengan nilai "favorite drink" dari properti object restaurant
+ */
+
+
+// TODO
+let restaurant = {
+	name: "Gigle",
+  	city: "Bandung",
+  	"favorite drink": "Orange Squash",
+  	"favorite food": "Beef Stroganoff",
+  	isVegan: false
+}
+
+let name;
+name = restaurant.name;
+let favoriteDrink;
+favoriteDrink = restaurant["favorite drink"];
+
+console.log(restaurant);
+console.log(name);
+console.log(favoriteDrink);
+
+/**
+ * Abaikan kode di bawah ini
+ */
+module.exports = { restaurant, name, favoriteDrink };
