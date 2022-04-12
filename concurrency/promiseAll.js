bf9a2783c5a62903db62a39b7ac1eb04b099aa10
Muhammad Aliansyah <aliansyaahian@gmail.com>
2022-04-12 22:28:21 +0700

    concurrency (promise all)

--- concurrency/promiseAll.js
+++ concurrency/promiseAll.js
@@ -1,3 +1,13 @@
+/* 
+    Promise All
+    Pada materi sebelumnya kita belajar bagaimana promise dapat menangani situasi di mana terdapat asynchronous process yang saling membutuhkan untuk melaksanakan tugasnya. 
+    
+    Lalu bagaimana jika kita ingin menjalankan banyak promise sekaligus tanpa memedulikan urutan? Bukankah concurrency memungkinkan kita melakukan banyak proses bersamaan agar lebih efisien?
+
+    Ketika pergi ke sebuah kedai kopi bersama rekan kerja, kita biasanya memesan kopi secara bersamaan. Meskipun kopi yang kita pesan berbeda, tak jarang pelayanan mengantarkan pesanan bersamaan. Nah, pada kasus inilah pelayan menggunakan teknik Promise.all().
+    
+*/
+
 // Objek untuk menyimpan state dari mesin kopi
 const state = {
     stock: {
