3d3a15b5089a9da06e3f02a3dbd3aebcff422ac1
Muhammad Aliansyah <aliansyaahian@gmail.com>
2022-06-11 11:30:19 +0700

    Concurrency (Consuming Promises)

--- concurrency/consumingPromises.js
+++ concurrency/consumingPromises.js
@@ -1,10 +1,26 @@
-// Consuming Promises
+/* 
+    Consuming Promises
+    Setelah mengetahui bagaimana membuat objek Promise, hal yang tentunya sangat penting adalah tahu bagaimana mengonsumsinya. Seperti yang telah kita bahas sebelumnya, status awal dari Promise adalah pending. Kemudian, akan ada dua kemungkinan yang terjadi, fulfilled atau rejected.
+
+    Untuk menangani hasil dari Promise, kita gunakan method .then(). Jika kita terjemahkan, “then” berarti “kemudian”, sehingga kurang lebih kita memerintahkan JavaScript seperti ini: “Jika janji saya sudah selesai, kemudian lakukan ...”. Jika dituliskan dalam bentuk kode akan seperti berikut:
+
+    const myPromise = new Promise(executorFunction);
+    myPromise.then(onFullfilled, onRejected);
+
+    .then() sendiri adalah sebuah higher-order function yang membutuhkan dua parameter. Keduanya adalah callback function yang juga dikenal sebagai handler. Handler pertama adalah fungsi yang akan dijalankan ketika Promise berstatus resolve. Sedangkan handler kedua adalah fungsi yang akan dijalankan ketika Promise berstatus reject.
+
+    Kembali ke kasus mesin kopi kita sebelumnya, mesin bisa gagal membuat kopi jika bahan-bahan tidak mencukupi. Sementara jika bahan cukup, mesin akan membuatkan satu gelas kopi. Di sinilah kita dapat memanfaatkan Promise sekaligus menangani dua kemungkinan promise yang terjadi.
+
+    Mari kita buat object untuk menyimpan stok dan fungsi yang mengembalikan objek Promise.
+*/
+
+// Object stock
 const stock = {
     coffeeBeans: 250,
     water: 1000,
 }
 
-// checkStock() merupakan fungsi yang mengembalikan promise dan akan menghasilkan resolve() dengan membawa nilai "Stok cukup. Bisa membuat kopi".
+// checkStock() merupakan fungsi yang mengembalikan PROMISE dan akan menghasilkan resolve() dengan membawa nilai "Stok cukup. Bisa membuat kopi".
 const checkStock = () => {
     return new Promise((resolve, reject) => {
         if (stock.coffeeBeans >= 16 && stock.water >= 250) {
@@ -17,11 +33,12 @@ const checkStock = () => {
 
 // Kita mendeklarasikan fungsi handleSuccess() dan handleFailure() yang mencetak nilai dari parameternya
 const handleSuccess = resolvedValue => {
-    console.log(resolvedValue);
+    // console.log(resolvedValue);
+    console.log("Berhasil: "+resolvedValue);
 }
  
 const handleFailure = rejectionReason => {
-    console.log(rejectionReason);
+    console.log("Gagal: "+rejectionReason);
 }
 
 // Memanggil method .then() dari checkStock
@@ -30,6 +47,6 @@ const handleFailure = rejectionReason => {
 checkStock().then(handleSuccess, handleFailure);
 
 // On rejected with catch method
-checkStock()
+/* checkStock()
     .then(handleSuccess)
-    .catch(handleFailure);
+    .catch(handleFailure); */
