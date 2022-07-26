5075b9314a48812e182464330c2db244b1456a5c
Muhammad Aliansyah <aliansyaahian@gmail.com>
2022-07-26 21:44:02 +0700

    Concurrency (async-await 2)

--- concurrency/async-await2.js
+++ concurrency/async-await2.js
@@ -1,80 +1,44 @@
-// Objek untuk menyimpan state dari mesin kopi
-const state = {
-    stock: {
-        coffeeBeans: 250,
-        water: 1000,
-    },
-    isCoffeeMachineBusy: false,
-}
+/* 
+    Handle onRejected using async-await
 
-// Promise checkAvailability
-const checkAvailability = () => {
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            if (!state.isCoffeeMachineBusy) {
-                resolve("Mesin kopi siap digunakan.");
-            } else {
-                reject("Maaf, mesin sedang sibuk.");
-            }
-        }, 1000);
-    });
-};
+    Perlu jadi catatan bahwa await hanya akan mengembalikan nilai jika promise berhasil dilakukan (onFulfilled). Lantas bagaimana jika promise gagal dilakukan (onRejected)? 
+    Caranya cukup sederhana. Kembali lagi kepada prinsip synchronous code. 
+    Kita dapat menangani sebuah eror atau tolakan dengan menggunakan try...catch.
 
-// Promise checkStock
-const checkStock = () => {
-    return new Promise((resolve, reject) => {
-        state.isCoffeeMachineBusy = true;
-        setTimeout(() => {
-            if (state.stock.coffeeBeans >= 16 && state.stock.water >= 250) {
-                resolve("Stok cukup. Bisa membuat kopi.");
-            } else {
-                reject("Stok tidak cukup!");
-            }
-        }, 1500);
-    });
-};
+    Ketika menggunakan async/await, biasakan ketika mendapatkan resolved value dari sebuah promise, untuk menempatkannya di dalam blok try seperti ini:
 
-// Promise boilWater
-const boilWater = () => {
-    return new Promise((resolve, reject) => {
-        console.log("Memanaskan air...");
-        setTimeout(() => {
-            resolve("Air panas sudah siap!");
-        }, 2000);
-    })
-}
-
-// Promise grindCoffeeBeans
-const grindCoffeeBeans = () => {
-    return new Promise((resolve, reject) => {
-        console.log("Menggiling biji kopi...");
-        setTimeout(() => {
-            resolve("Biji kopi sudah siap!");
-        }, 1000);
-    })
-}
-
-// Promise brewCoffee
-const brewCoffee = () => {
-    console.log("Membuatkan kopi Anda...")
-    return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            resolve("Kopi sudah siap!")
-        }, 2000);
-    });
-};
-
-// Chaining Promise using async-await
-async function makeEspresso() {
+    async function makeCoffee() {
     try {
-        await checkAvailability();
-        await checkStock();
-        await Promise.all([boilWater(), grindCoffeeBeans()]);
-        const coffee = await brewCoffee();
+        const coffee = await getCoffee();
+        console.log(coffee);
+    }
+
+    Dengan begitu kita dapat menggunakan blok catch untuk menangani jika promise gagal dilakukan (onRejected).
+}
+*/
+
+const getCoffee = () => {
+    return new Promise((resolve, reject) => {
+        const seeds = 100;
+        setTimeout(() => {
+            if (seeds >= 10) {
+                resolve("Kopi didapatkan!");
+            } else {
+                reject("Biji kopi habis!");
+            }
+        }, 1000);
+    })
+}
+
+async function makeCoffee() {
+    try {
+        // Biasakan ketika mendapatkan resolved value dari sebuah promise, kita tempatkan di dalam blok try
+        const coffee = await getCoffee();
         console.log(coffee);
     } catch (rejectedReason) {
+        // Agar kita dapat menggunakan blok catch utk menangani jika promise gagal dilakukan
         console.log(rejectedReason);
     }
 }
-
-makeEspresso();
+ 
+makeCoffee();
