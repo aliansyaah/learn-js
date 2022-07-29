3e33787541d98cb4c5693b597c508c95d98d8380
Muhammad Aliansyah <aliansyaahian@gmail.com>
2022-07-29 23:31:27 +0700

    Javascript Testing

--- latihan-testing/__tests__/gradeCalculations.test.js
+++ latihan-testing/__tests__/gradeCalculations.test.js
@@ -9,10 +9,14 @@ describe('grade calculations', () => {
     test('it should handle non-number ', () => {
         const listValueOfExams = [80, 'a', '100', 80];
         expect(() => averageExams(listValueOfExams)).toThrow();
+        // expect(() => averageExams(listValueOfExams)).toThrow(Error);
+        // expect(() => averageExams(listValueOfExams)).toThrow('please input number');
     });
 
     /**
      * Integration testing
+     * 
+     * Integration test dapat dijalankan ataupun ditulis bersamaan dengan unit test. Sehingga kita dapat melakukan grouping dari contoh kode diatas menjadi sebagai berikut:
      */
     test('it should return exam passed status', () => {
         const listValueOfExams = [80, 100, 100, 80];
@@ -21,6 +25,6 @@ describe('grade calculations', () => {
 
     test('it should return exam failed status', () => {
         const listValueOfExams = [50, 40, 70, 80];
-        expect(isStudentPassExam(listValueOfExams, 'Budi')).toEqual(false);
+        expect(isStudentPassExam(listValueOfExams, 'Dodo')).toEqual(false);
     });
 });
--- latihan-testing/gradeCalculations.js
+++ latihan-testing/gradeCalculations.js
@@ -1,3 +1,13 @@
+/* 
+    Jest
+    Dalam penulisan kode pengujian, kita dapat menggunakan library tambahan untuk mempermudah penulisan kode pengujian.
+
+    Jest merupakan salah satu framework testing paling populer untuk menuliskan kode pengujian pada bahasa pemrograman JavaScript. Jest dapat digunakan untuk menuliskan script testing pada aplikasi backend maupun frontend.
+
+    Kita akan mencoba untuk menuliskan sebuah kode pengujian menggunakan framework jest.
+*/
+
+// Menghitung rata-rata nilai siswa dari sebuah inputan berupa daftar angka dalam bentuk array
 const averageExams = (valuesExam) => {
     const numberValidation = valuesExam.every(exam => typeof exam === 'number');
     if (!numberValidation) throw Error('please input number');
@@ -5,7 +15,8 @@ const averageExams = (valuesExam) => {
     const sumValues = valuesExam.reduce((accumulator,currentValue) => accumulator + currentValue, 0);
     return sumValues / valuesExam.length;
 };
- 
+
+// Melakukan kalkulasi apakah seorang siswa lulus ujian atau tidak berdasarkan nilai rata-rata yang didapatkan (bergantung pada fungsi averageExams).
 const isStudentPassExam = (valuesExam, name) => {
     const minValues = 75;
     const average = averageExams(valuesExam);
