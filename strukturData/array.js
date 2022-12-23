832c7177c94fb2e6fb96d9ee65c879cec457af1a
Muhammad Aliansyah <aliansyah@Muhammads-MacBook-Pro.local>
2022-12-23 22:51:19 +0700

    Enter new line comment

--- strukturData/array.js
+++ strukturData/array.js
@@ -1,7 +1,11 @@
 /* 
-    Array merupakan tipe data yang dapat mengelompokkan lebih dari satu nilai dan menempatkannya dalam satu variabel.
+    Array merupakan tipe data yang dapat mengelompokkan lebih dari satu nilai 
+    dan menempatkannya dalam satu variabel.
 
-    Perbedaan array dengan object adalah data pada array disusun secara berurutan dan diakses menggunakan index. Untuk mengakses nilai di dalam array, kita gunakan tanda kurung siku [] yang di dalamnya berisi angka yang merupakan posisi nilai yang ingin diakses.
+    Perbedaan array dengan object adalah data pada array disusun secara berurutan 
+    dan diakses menggunakan index. 
+    Untuk mengakses nilai di dalam array, kita gunakan tanda kurung siku [] yang 
+    di dalamnya berisi angka yang merupakan posisi nilai yang ingin diakses.
 */
 let myArray = ["Cokelat", 42.5, 22, true, "Programming"];
 console.log(myArray);
